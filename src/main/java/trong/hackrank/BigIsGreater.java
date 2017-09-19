b28ae2260c6dfea80c71a67365f9cd278ba6741e
SOGOU-INC\rongbin <rongbin@sogou-inc.com>
2017-09-19 17:09:54 +0800

    nothing

--- src/main/java/trong/hackrank/BigIsGreater.java
+++ src/main/java/trong/hackrank/BigIsGreater.java
@@ -8,51 +8,55 @@ import java.util.Scanner;
 
 public class BigIsGreater {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int num = scanner.nextInt();
-    for (int i = 0; i <= num - 1; i++) {
-      char[] letters = scanner.nextLine().toCharArray();
-      boolean isNotLetter = false;
-      for (int n = 0; n < letters.length - 1; n++) {
-        // check the input is low case letter
-        if (letters[n] < 97 || letters[n] > 122) {
-          isNotLetter = true;
-          break;
-        }
-      }
-      if (isNotLetter) {
-        break;
+    try {
+      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+      int num = Integer.parseInt(in.readLine());
+//      System.out.println(num);
+
+      String[] words = new String[num];
+      for (int i = 0; i <= num-1; i++) {
+        words[i] = in.readLine();
       }
 
-      // lexicographical sort
-      int position = -1;
-      for (int n = letters.length - 1; n >= 0; n--) {
-        for (int m = n - 1; m >= 0; m--) {
-          if (letters[m] < letters[n]) {
-            position = m;
-            // swap the two letter
-            char tmp = letters[m];
-            letters[m] = letters[n];
-            letters[n] = tmp;
+      String input = "";
+      for (int i = 0; i <= num-1; i++) {
+        input = words[i];
+        char[] letters = input.toCharArray();
+
+        // lexicographical sort
+        int position = -1;
+        for (int n = letters.length - 1; n >= 0; n--) {
+          for (int m = n - 1; m >= 0; m--) {
+            if (letters[m] < letters[n]) {
+              position = m;
+              // swap the two letter
+              char tmp = letters[m];
+              letters[m] = letters[n];
+              letters[n] = tmp;
+              break;
+            }
+          }
+          if (position != -1) {
             break;
           }
         }
-        if (position != -1) {
-          break;
+
+        if (position == -1) {
+          System.out.print("no answer");
+          System.out.println();
+        } else {
+          char[] beforeP = Arrays.copyOfRange(letters, 0, position + 1);
+          char[] afterP = Arrays.copyOfRange(letters, position + 1, letters.length);
+          System.out.print(beforeP);
+          Arrays.sort(afterP);
+          System.out.print(afterP);
+          System.out.println();
         }
       }
 
-      if (position == -1) {
-        System.out.print("no answer");
-        System.out.println();
-      } else {
-        char[] beforeP = Arrays.copyOfRange(letters, 0, position + 1);
-        char[] afterP = Arrays.copyOfRange(letters, position + 1, letters.length);
-        System.out.print(beforeP);
-        Arrays.sort(afterP);
-        System.out.print(afterP);
-        System.out.println();
-      }
+      in.close();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
 }
