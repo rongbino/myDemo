3c83a73e2d96f394333465a3e8f31bce77712b0b
SOGOU-INC\rongbin <rongbin@sogou-inc.com>
2017-09-19 18:57:24 +0800

    new code

--- src/main/java/trong/hackrank/BigIsGreater.java
+++ src/main/java/trong/hackrank/BigIsGreater.java
@@ -1,8 +1,6 @@
 package trong.hackrank;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -11,12 +9,12 @@ public class BigIsGreater {
     try {
       BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
       int num = Integer.parseInt(in.readLine());
-//      System.out.println(num);
 
       String[] words = new String[num];
       for (int i = 0; i <= num-1; i++) {
         words[i] = in.readLine();
       }
+      in.close();
 
       String input = "";
       for (int i = 0; i <= num-1; i++) {
@@ -24,37 +22,36 @@ public class BigIsGreater {
         char[] letters = input.toCharArray();
 
         // lexicographical sort
-        int position = -1;
-        for (int n = letters.length - 1; n >= 0; n--) {
-          for (int m = n - 1; m >= 0; m--) {
-            if (letters[m] < letters[n]) {
-              position = m;
-              // swap the two letter
-              char tmp = letters[m];
-              letters[m] = letters[n];
-              letters[n] = tmp;
-              break;
-            }
+        int start = -1;
+        // find the first different
+        for (int n = 0; n < letters.length-1; n++) {
+          if (letters[n] < letters[n+1]) {
+            start = n;
           }
-          if (position != -1) {
-            break;
-          }
+        }
+        if (start == -1) {
+          System.out.println("no answer");
+          continue;
         }
 
-        if (position == -1) {
-          System.out.print("no answer");
-          System.out.println();
-        } else {
-          char[] beforeP = Arrays.copyOfRange(letters, 0, position + 1);
-          char[] afterP = Arrays.copyOfRange(letters, position + 1, letters.length);
-          System.out.print(beforeP);
-          Arrays.sort(afterP);
-          System.out.print(afterP);
-          System.out.println();
+        int end = -1;
+        // find the smallest letter
+        for (int n = start + 1; n <= letters.length-1; n++) {
+          if (letters[start] < letters[n]) {
+            end = n;
+          }
         }
+        char tmp = letters[end];
+        letters[end] = letters[start];
+        letters[start] = letters[end];
+        char[] beforeP = Arrays.copyOfRange(letters, 0, start + 1);
+        char[] afterP = Arrays.copyOfRange(letters, start + 1, letters.length);
+        System.out.print(beforeP);
+        Arrays.sort(afterP);
+        System.out.print(afterP);
+        System.out.println();
       }
 
-      in.close();
     } catch (Exception e) {
       e.printStackTrace();
     }
