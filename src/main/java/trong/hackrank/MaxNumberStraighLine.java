4742e5a4119e85ad9300fefead7522ebd0e49059
Tommy Rong <tommy.rong.cn@gmail.com>
2017-09-22 00:12:32 +0800

    max number on straight line

---
+++ src/main/java/trong/hackrank/MaxNumberStraighLine.java
@@ -0,0 +1,38 @@
+package trong.hackrank;
+
+/**
+ * Given n points on a 2D plane,
+ * find the maximum number of points that lie on the same straight line
+ */
+public class MaxNumberStraighLine {
+
+  public int maxPoints(Point[] points) {
+    // the max number
+    int result = 0;
+    for (int i = 0; i <= points.length; i++) {
+      Point start = points[i];
+      for ()
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
+
+class Point {
+  int x;
+  int y;
+
+  Point() {
+    x = 0;
+    y = 0;
+  }
+
+  Point(int a, int b) {
+    x = a;
+    y = b;
+  }
+}
