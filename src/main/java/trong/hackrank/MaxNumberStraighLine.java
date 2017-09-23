9d4dc94dd9aa5546e5559de17e63fd36499eb93f
Tommy Rong <tommy.rong.cn@gmail.com>
2017-09-23 23:26:43 +0800

    max points on the same straight line

--- src/main/java/trong/hackrank/MaxNumberStraighLine.java
+++ src/main/java/trong/hackrank/MaxNumberStraighLine.java
@@ -1,5 +1,7 @@
 package trong.hackrank;
 
+import java.util.HashMap;
+
 /**
  * Given n points on a 2D plane,
  * find the maximum number of points that lie on the same straight line
@@ -7,18 +9,67 @@ package trong.hackrank;
 public class MaxNumberStraighLine {
 
   public int maxPoints(Point[] points) {
+    if (points.length == 0) {
+      return 0;
+    }
+    if (points.length == 1) {
+      return 1;
+    }
+
     // the max number
     int result = 0;
-    for (int i = 0; i <= points.length; i++) {
-      Point start = points[i];
-      for ()
+    for (int i = 0; i < points.length; i++) {
+      int vcnt = 0, dup = 0, curmax =1; //垂直一条线的点，重合点，当前共线的最大点
+
+
+      HashMap<Double, Integer> map = new HashMap<>();
+      for (int j = i+1; j < points.length; j++) {
+        double delta_x = 0, delta_y=0, k=0.0;
+        if (j != i) {
+          delta_x = points[i].x - points[j].x;
+          delta_y = points[i].y - points[j].y;
+          if (delta_x == 0 && delta_y == 0) {
+            dup++;
+          }
+          else if (delta_x == 0) {
+            if (vcnt == 0) {
+              vcnt = 2;
+            }
+            else {
+              vcnt++;
+            }
+            curmax = Math.max(vcnt, curmax);
+          }
+          else {
+            k = delta_y/delta_x;
+            if (map.get(k) == null) {
+              map.put(k, 2);
+            }
+            else {
+              Integer or = map.get(k);
+              or++;
+              map.put(k, or);
+            }
+            curmax = Math.max(map.get(k), curmax);
+          }
+        }
+      }
+      result = Math.max(result, curmax+dup);
     }
 
     return result;
   }
 
   public static void main(String[] args) {
-
+    Point[] test = new Point[3];
+    Point a = new Point(0,0);
+    Point b = new Point(94911151,94911150);
+    Point c = new Point(94911152,94911151);
+    test[0] = a;
+    test[1] = b;
+    test[2] = c;
+    MaxNumberStraighLine ss = new MaxNumberStraighLine();
+    System.out.println(ss.maxPoints(test));
   }
 }
 
