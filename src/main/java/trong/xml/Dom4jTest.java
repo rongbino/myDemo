fbcdd843b95201a4e5245fefc32509fdea383113
SOGOU-INC\rongbin <rongbin@sogou-inc.com>
2018-03-14 11:26:02 +0800

    add the spring boot dependence

--- src/main/java/trong/xml/Dom4jTest.java
+++ src/main/java/trong/xml/Dom4jTest.java
@@ -1,11 +1,9 @@
 package trong.xml;
 
-import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.dom4j.io.XMLWriter;
-import scala.xml.XML;
 
 public class Dom4jTest {
   public static void main(String[] args) {
@@ -15,7 +13,6 @@ public class Dom4jTest {
     root.addAttribute("name", "name_value");
     Element el = root.addElement("element");
     el.addElement("name2");
-
 
     XMLWriter writer = new XMLWriter();
     String header = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\" ?>";
