c94b9574514f340ae22589dadd9e6a0f5604c099
SOGOU-INC\rongbin <rongbin@sogou-inc.com>
2018-06-21 11:15:36 +0800

    complete the doubly linked list

--- src/main/java/trong/alg/list/CustomerDoublyLinkedList.java
+++ src/main/java/trong/alg/list/CustomerDoublyLinkedList.java
@@ -1,5 +1,7 @@
 package trong.alg.list;
 
+import java.util.LinkedList;
+
 public class CustomerDoublyLinkedList<E> {
   // size of Doubly linked
   transient int size = 0;
@@ -9,6 +11,67 @@ public class CustomerDoublyLinkedList<E> {
   transient Node<E> last;
 
   public CustomerDoublyLinkedList() {}
+
+  /**
+   * Link the node after last one
+   * @param element
+   */
+  public void link(E element) {
+    if (first == null) {
+      final Node<E> e = new Node<>(null, element, null);
+      first = e;
+      last = e;
+      size++;
+    } else {
+      final Node<E> e = new Node<>(null, element, null);
+      last.next = e;
+      e.pre = last;
+      // last move next
+      last = e;
+      size++;
+    }
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public E node(int index) {
+    Node<E> node = first;
+    for (int i = 0; i < size; i++) {
+      if (i < index) {
+        node = node.next;
+      } else {
+        break;
+      }
+    }
+    return node.element;
+  }
+
+  /**
+   * remove the first one
+   */
+  public E remove() {
+    if (first == null) {
+      return null;
+    }
+
+    if (first == last) {
+      size--;
+      E val = first.element;
+      first = null;
+      last = null;
+      return val;
+    }
+
+    // more than one
+    Node<E> eNode = first;
+    final E element  = eNode.element;
+    first = first.next;
+    eNode = null; // help GC
+    size--;
+    return element;
+  }
 
   /**
    * Node class
--- src/main/java/trong/alg/list/ListTest.java
+++ src/main/java/trong/alg/list/ListTest.java
@@ -5,12 +5,31 @@ import java.util.LinkedList;
 public class ListTest {
   public static void main(String[] args) {
     ListTest test = new ListTest();
-//    test.testLinkedList();
+    test.testLinkedList();
     test.testCircularList();
+    test.testDoublyList();
   }
 
 
+  public void testDoublyList() {
+    System.out.println("======= Doubly list test ======");
+    CustomerDoublyLinkedList<Integer> customerDoublyLinkedList = new CustomerDoublyLinkedList<>();
+    customerDoublyLinkedList.link(0);
+    customerDoublyLinkedList.link(1);
+    customerDoublyLinkedList.link(2);
+    customerDoublyLinkedList.link(3);
+    // print init doubly list
+    printList(customerDoublyLinkedList);
+    System.out.printf("Delete first %d, the size change to %d\n", customerDoublyLinkedList.remove(), customerDoublyLinkedList.size());
+    System.out.printf("Delete first %d, the size change to %d\n", customerDoublyLinkedList.remove(), customerDoublyLinkedList.size());
+    System.out.printf("Delete first %d, the size change to %d\n", customerDoublyLinkedList.remove(), customerDoublyLinkedList.size());
+    System.out.printf("Delete first %d, the size change to %d\n", customerDoublyLinkedList.remove(), customerDoublyLinkedList.size());
+
+    printList(customerDoublyLinkedList);
+  }
+
   public void testCircularList() {
+    System.out.println("======= Circular list test ======");
     CustomerCircularLinedList<Integer> customerCircularLinedList = new CustomerCircularLinedList<>();
     customerCircularLinedList.link(0);
     customerCircularLinedList.link(1);
@@ -28,6 +47,7 @@ public class ListTest {
    * make test for linked list
    */
   public void testLinkedList() {
+    System.out.println("======= Linked list test ======");
     CustomerLinkedList<Integer> customerLinkedList = new CustomerLinkedList<>();
     customerLinkedList.linkLast(0);
     customerLinkedList.linkLast(1);
@@ -56,6 +76,13 @@ public class ListTest {
   }
 
   public void printList(CustomerCircularLinedList l) {
+    for (int i = 0; i < l.size; i++) {
+      System.out.print(" " + l.node(i));
+    }
+    System.out.println();
+  }
+
+  public void printList(CustomerDoublyLinkedList l) {
     for (int i = 0; i < l.size; i++) {
       System.out.print(" " + l.node(i));
     }
