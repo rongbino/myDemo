79048d33cb97c7b5b2083942d58611dc61adaaa1
Bin Rong <rongbin@Bins-MBP.hitronhub.home>
2019-11-11 13:59:11 -0700

    merge two type object into json object

--- src/main/java/trong/json/JsonTest.java
+++ src/main/java/trong/json/JsonTest.java
@@ -3,6 +3,13 @@ package trong.json;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JsonTest {
     public static void main(String[] args) {
         JsonArray array = new JsonArray();
@@ -12,5 +19,27 @@ public class JsonTest {
         array.add(obj);
 
         System.out.println(array.get(array.size()-1).getAsJsonObject().get("num").getAsInt());
+
+        HashMap<String, Object> f = new HashMap<>();
+
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+        HashMap<String, Map<String, String>>  map = new HashMap<>();
+        HashMap<String, String> vl = new HashMap<>();
+        vl.put("key", "value");
+        map.put("map", vl);
+
+        f.put("list", list);
+        f.put("enum", map);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            System.out.println(objectMapper.writeValueAsString(f));
+        } catch (Exception e) {
+            e.toString();
+        }
     }
 }
